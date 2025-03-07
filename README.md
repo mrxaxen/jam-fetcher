4f67b5c81f12202b2664abb92d9a323c8709ea72
Eszter, Gábor <mrxaxen@gmail.com>
2025-03-07 16:53:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Jam Fetcher
+
+## Functions
+
+- Pass a list of playlist urls to it which you want downloaded overtime
+- Downloads the playlist overtime and keeps it fresh(no deletes, just additions to keep it safe)
---
+++ install-ytdlp.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl -L https://github.com/yt-dlp/yt-dlp/raw/master/public.key | gpg --import
+gpg --verify SHA2-256SUMS.sig SHA2-256SUMS
+gpg --verify SHA2-512SUMS.sig SHA2-512SUMS
+
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
