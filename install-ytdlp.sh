c81f71ece03dd1ce3aab9dea102c5f955ba11484
Eszter, Gábor <mrxaxen@gmail.com>
2025-03-08 17:17:59 +0100

    v1 downloader

--- install-ytdlp.sh
+++ install-ytdlp.sh
@@ -5,3 +5,5 @@ gpg --verify SHA2-256SUMS.sig SHA2-256SUMS
 gpg --verify SHA2-512SUMS.sig SHA2-512SUMS
 
 wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+chmod 555 ./yt-dlp
+mv ./yt-dlp /usr/bin/
--- main.sh
+++ main.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+PLAYLIST_FILE="playlists"
+DOWNLOAD_ROOT_PATH="."
+
+while IFS="," read -r playlist_name url; do
+    DOWNLOAD_PATH="${DOWNLOAD_ROOT_PATH}/${playlist_name}"
+    if [[ ! -d $DOWNLOAD_PATH ]]; then
+        mkdir -p ${DOWNLOAD_PATH}/logs
+    fi
+    yt-dlp $url \
+        --yes-playlist \
+        --restrict-filenames \
+        --no-abort-on-error \
+        -o $DOWNLOAD_PATH \
+        --download-archive "${playlist_name}_archive" \
+        -x \
+        --audio-quality 0 |
+        tee "${DOWNLOAD_PATH}/logs/progress-$(date +%Y-%m-%d_%H-%M-%S).log"
+done <$PLAYLIST_FILE
